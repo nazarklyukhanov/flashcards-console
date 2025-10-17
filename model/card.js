c976b35ca5d7b54b2e76202e6cc6bdffa458d50e
bulbenko2 <dsnbel@gmail.com>
2025-10-17 11:24:20 +0300

    test1

--- model/card.js
+++ model/card.js
@@ -1,28 +1,13 @@
-
 class Card {
-  constructor({ id, front, back, options, question, correct, choices } = {}) {
-
-    this.front = front ?? question ?? '';
-    this.back = back ?? correct ?? '';
-
-    if (Array.isArray(options)) {
-      this.options = options.slice();
-    } else if (Array.isArray(choices)) {
-      this.options = choices.slice();
-    } else {
-      this.options = null;
-    }
-
-    this.id = String(id);
-
-    this.attempts = 0;
-    this.correct = false;
-    this.lastAnswer = null;
+  constructor(question, choices, correct ) {
+    this.question = question
+    this.choices = choices
+    this.correct = correct
   }
 
   reset() {
     this.attempts = 0;
-    this.correct = false;
+    this.isGuessed = false;
     this.lastAnswer = null;
   }
 }
