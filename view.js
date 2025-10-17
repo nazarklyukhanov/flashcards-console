5d6b506968f907c8037cb32aa95a90906d995df3
deminvv1 <deminvladtema@mail.ru>
2025-10-17 12:47:38 +0300

    vlad-view1

--- view.js
+++ view.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const figlet = require('figlet');
 const chalk = require('chalk');
+const { EOL } = require('os')
 
 class View {
     static async getUserInfo(themes) {
@@ -10,10 +11,40 @@ class View {
       {
         name: 'theme',
         type: 'list',
-        message: 'Выберите одну из предложенных тем:',
+        message: 'Выберите тему для решения:',
         choices: themes,
       },
     ]);
   }
 
+  static async greetUser(username) {
+    const text = figlet.textSync(` ${EOL}Хорошей игры, дорогой друг, ${username} !`, {
+      font: 'ANSI Shadow',
+      horizontalLayout: 'default',
+      verticalLayout: 'default',
+      whitespaceBreak: true,
+    });
+
+    const padded = text
+      .split('\n')
+      .map((line) => '    ' + line)
+      .join('\n');
+
+    console.log(chalk.green(padded));
+  }
+
+  static async greetings() {
+    const data = await new Promise((resolve, reject) => {
+      figlet('Flashcards Game', { font: 'ANSI Shadow' }, (err, txt) =>
+        err ? reject(err) : resolve(txt),
+      );
+    });
+
+    const lines = data.split('\n');
+    for (const line of lines) {
+      console.log(chalk.hex('#fff87dff')(line));
+      await new Promise((r) => setTimeout(r, 200));
+    }
+
+}
 }
